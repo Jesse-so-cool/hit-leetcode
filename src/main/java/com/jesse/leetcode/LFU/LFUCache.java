4c4c24f03dde4354a49f7ad11f7c822b490f40a3
黄时捷 <huangshijie2@bluemoon.com.cn>
2021-03-19 09:37:44 +0800

    LFU

--- src/main/java/com/jesse/leetcode/LFU/LFUCache.java
+++ src/main/java/com/jesse/leetcode/LFU/LFUCache.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- *
+ * LFU -> 对应原理图
  * @author jesse hsj
  * @date 2021/3/15 16:36
  */
@@ -12,27 +12,31 @@ public class LFUCache {
 
     public static void main(String[] args) {
         LFUCache lruCache = new LFUCache(2);
-        lruCache.put(1,1);
-        lruCache.put(2,2);
-        lruCache.get(1);
-        lruCache.put(3,3);
-        lruCache.get(2);
-        lruCache.get(3);
-        lruCache.put(4,4);
-        lruCache.get(1);
-        lruCache.get(3);
-        lruCache.get(4);
+        lruCache.put(1, 1);
+        lruCache.put(2, 2);
+        System.out.println(lruCache.get(1));
+        lruCache.put(3, 3);//2被淘汰
+        System.out.println(lruCache.get(2));//-1
+        System.out.println(lruCache.get(3));//freq2 1,3
+        lruCache.put(4, 4);//淘汰1
+        System.out.println(lruCache.get(1));
+        System.out.println(lruCache.get(3));
+        System.out.println(lruCache.get(4));
     }
 
     int capacity;
-    Map<Integer,Node<Integer,Integer>> cache = new HashMap<>();
-    Map<Integer,DoubleNodeList> frequentMap = new HashMap<>();
+    Map<Integer, Node<Integer, Integer>> cache = new HashMap<>();
+    Map<Integer, DoubleNodeList> frequentMap = new HashMap<>();
     int minFrequent;
+
     public LFUCache(int capacity) {
         this.capacity = capacity;
     }
 
     public int get(int key) {
+        if (capacity == 0) {
+            return -1;
+        }
         if (cache.containsKey(key)) {
             Node<Integer, Integer> node = cache.get(key);
 
@@ -42,7 +46,7 @@ public class LFUCache {
             joinNewFreqList(node);
 
             return node.value;
-        }else {
+        } else {
             return -1;
         }
     }
@@ -50,16 +54,17 @@ public class LFUCache {
     private void joinNewFreqList(Node<Integer, Integer> node) {
         DoubleNodeList doubleNodeList;
         //加入新的freq链表
-        if (frequentMap.containsKey(node.freq)){
+        if (frequentMap.containsKey(node.freq)) {
             doubleNodeList = frequentMap.get(node.freq);
-        }else {
+        } else {
             doubleNodeList = new DoubleNodeList();
         }
         node.prev = doubleNodeList.head;
         node.next = doubleNodeList.head.next;
         doubleNodeList.head.next.prev = node;
         doubleNodeList.head.next = node;
-        frequentMap.put(node.freq,doubleNodeList);
+        doubleNodeList.count++;
+        frequentMap.put(node.freq, doubleNodeList);
     }
 
     private void clearOldFreqList(Node<Integer, Integer> node) {
@@ -71,27 +76,30 @@ public class LFUCache {
         frequentMap.get(node.freq).count--;
 
         //修改minfreq
-        if (node.freq == minFrequent && frequentMap.get(node.freq).count == 0){
+        if (node.freq == minFrequent && frequentMap.get(node.freq).count == 0) {
             minFrequent++;
         }
     }
 
     public void put(int key, int value) {
-        if (cache.containsKey(key)){
+        if (capacity == 0) {
+            return;
+        }
+        if (cache.containsKey(key)) {
             Node<Integer, Integer> node = cache.get(key);
             node.value = value;
             clearOldFreqList(node);
             node.freq++;
             joinNewFreqList(node);
 
-        }else {
-            if (cache.size() == capacity){
+        } else {
+            if (cache.size() == capacity) {
                 removeLFU();
             }
-            Node<Integer, Integer> node = new Node<>(key,value);
+            Node<Integer, Integer> node = new Node<>(key, value);
 
 
-            cache.put(key,node);
+            cache.put(key, node);
             joinNewFreqList(node);
             minFrequent = 1;
         }
@@ -99,22 +107,23 @@ public class LFUCache {
 
     private void removeLFU() {
         DoubleNodeList nodeList = frequentMap.get(minFrequent);
-        if (nodeList.count == 1){
-            cache.remove(nodeList.head.next.key);
+        if (nodeList.count == 1) {
+            cache.remove(nodeList.tail.prev.key);
             frequentMap.remove(minFrequent);
-
-        }else {
-            nodeList.tail.prev = nodeList.tail.prev.prev;
+        } else {
+            cache.remove(nodeList.tail.prev.key);
             nodeList.tail.prev.prev.next = nodeList.tail;
-
+            nodeList.tail.prev = nodeList.tail.prev.prev;
+            nodeList.count--;
         }
     }
+
     private class Node<K, V> {
         K key;
         V value;
         int freq = 1;
-        Node<K,V> prev;
-        Node<K,V> next;
+        Node<K, V> prev;
+        Node<K, V> next;
 
         public Node(K key, V value) {
             this.key = key;
@@ -125,7 +134,7 @@ public class LFUCache {
         }
     }
 
-    public class DoubleNodeList{
+    public class DoubleNodeList {
         int count = 0;
         Node head;
         Node tail;
